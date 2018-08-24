6c30a12916c48c97b4ae5a14231e32238da80c1f
Jaerin Kim <36051223+jaerinkim@users.noreply.github.com>
2018-08-24 02:05:04 -0500

    Sort functions work now

--- homeworks/hw3/jaerin.hw3.py
+++ homeworks/hw3/jaerin.hw3.py
@@ -20,8 +20,8 @@ def mostPopular(groups):
 # 1.'Sip, Socialize, and Get Creative in STL!' is the most
 # popular group with 5323 members, as of now
 
-print mostPopular(musicgroups).name
-print mostPopular(musicgroups).members
+mmp=mostPopular(musicgroups)
+print mmp.name,'\n' ,mmp.members
 
 # 2. First, GetIDs fundtion returns IDs of group members,
 # given a group meetup object as an input
@@ -29,9 +29,7 @@ print mostPopular(musicgroups).members
 def getIDs(group):
     users=api.GetMembers({"group_urlname":group.urlname})
     output=[]
-    for i in users.__dict__["results"]:
-        output.append(i["id"])
-    return output
+    return [i["id"] for i in users.__dict__["results"]]
 
 # getUserGroups takes a list of ID as an input,
 # and returns a dictionary of lists containing each member's groups
--- homeworks/hw4/jaerin.hw4.py
+++ homeworks/hw4/jaerin.hw4.py
@@ -1,19 +1,18 @@
 def shortersort(listinput):
+    temp=listinput[:]
     output=[]
-    for i in range(len(listinput)):
-        output.append(min(listinput))
-        listinput.remove(min(listinput))
+    for i in range(len(temp)):
+        output.append(min(temp))
+        temp.remove(min(temp))
     return(output)
 
 def longersort(listinput):
-    changes=True
-    while changes==True:
-        changes=False
-        for i in range(len(listinput)-1):
-            for j in range(len(listinput)-1):
-                if listinput[i]>listinput[j+1]:
-                    listinput.insert(j+1,listinput[i])
-                    listinput.remove(listinput[i])
-                    changes=True
-                    break
-    return(listinput)
+    if len(listinput)>2:
+        temp=[listinput[0]]+longersort(listinput[1:])
+    else:
+        temp=listinput
+    for i,value in enumerate(temp[1:]):
+        if listinput[0]>value:
+            temp.insert(i+2,temp[i])
+            del temp[i]
+    return(temp)
